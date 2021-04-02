3d056b2de59bd23ae15add8ead537d7aea5ebadc
Giuseppe <giuseppe.figliuolo@mono-grid.com>
2021-04-02 09:58:41 +0200

    data add to js

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+const data = {
+  andromeda: {
+    title: "Gal. di Andromeda",
+    description:
+      "La Galassia di Andromeda è una galassia a spirale gigante che dista circa 2,538 milioni di anni luce dalla Terra in direzione della costellazione di Andromeda, da cui prende il nome. Si tratta della galassia di grandi dimensioni più vicina alla nostra, la Via Lattea; è visibile anche a occhio nudo ed è tra gli oggetti più lontani visibili senza l'ausilio di strumenti.",
+  },
+  backwards: {
+    title: "NGC 4622",
+    description:
+      "Una magnifica galassia a spirale, NGC 4622, distante circa 200 milioni di anni luce in direzione della costellazione del Centauro, sta confondendo le idee agli astronomi. Le osservazioni effettuate dal telescopio spaziale “Hubble” (HST) mostrano infatti che una parte del disco di questa galassia ruota attorno al nucleo in senso opposto rispetto alla norma.",
+  },
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector(".webgl");
   const galaxyP = document.querySelector(".galaxy-description");
-  const itineraryBtn = document.querySelector(".galaxy-description .button");
   const countdown = document.querySelector(".countdown");
+  const galBtn = document.querySelector("#next-galaxy");
 
   // landing page animation
   gsap
@@ -45,7 +58,12 @@ document.addEventListener("DOMContentLoaded", () => {
     startCountdown();
   });
 
-  itineraryBtn.addEventListener("click", () => {
-    console.log("ciao");
+  galBtn.addEventListener("click", () => {
+    console.log(data);
   });
+
+  // next galaxy function
+  const nextGalaxy = () => {
+    console.log();
+  };
 });
